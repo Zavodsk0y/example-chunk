351bc12f022f6bf4e6c06a2bf47435679ace0a1c
rayde03 <barchugova03@bk.ru>
2024-04-19 10:46:31 +0700

    1

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Pion\Laravel\ChunkUpload\Exceptions\UploadMissingFileException;
 use Pion\Laravel\ChunkUpload\Handler\HandlerFactory;
 use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
 
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,48 +1,59 @@
-<template>
-  <div>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div id="app">
     <input type="file" @change="selectFile">
     <button @click="uploadFile">Upload</button>
     <div v-if="uploadProgress !== null">
-      Upload progress: {{ uploadProgress }}%
     </div>
-  </div>
-</template>
+</div>
+</body>
+</html>
 
+<script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 <script>
-export default {
-  data() {
-    return {
-      file: null,
-      uploadProgress: null
-    };
-  },
-  methods: {
-    selectFile(event) {
-      this.file = event.target.files[0];
-    },
-    async uploadFile() {
-      const formData = new FormData();
-      formData.append('file', this.file);
+    new Vue({
+        el: "#app",
+        data() {
+            return {
+                file: null,
+                uploadProgress: null
+            };
+        },
+        methods: {
+            selectFile(event) {
+                this.file = event.target.files[0];
+            },
+            async uploadFile() {
+                const formData = new FormData();
+                formData.append('file', this.file);
 
-      try {
-        const response = await fetch('/upload', {
-          method: 'POST',
-          body: formData,
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        });
+                try {
+                    const response = await fetch('/upload', {
+                        method: 'POST',
+                        body: formData,
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    });
 
-        const data = await response.json();
-        if (data.success) {
-          console.log('File uploaded successfully');
-        } else {
-          console.error('Upload failed');
+                    const data = await response.json();
+                    if (data.success) {
+                        console.log('File uploaded successfully');
+                    } else {
+                        console.error('Upload failed');
+                    }
+                } catch (error) {
+                    console.error('Error uploading file:', error);
+                }
+            }
         }
-      } catch (error) {
-        console.error('Error uploading file:', error);
-      }
-    }
-  }
-};
+    });
 </script>
