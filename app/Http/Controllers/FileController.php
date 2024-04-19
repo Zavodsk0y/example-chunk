0b760249e7e8c5c9d017e85f71948f39bde744f0
Constantine <81346637+Zavodsk0y@users.noreply.github.com>
2024-04-19 06:38:48 +0300

    Update FileController.php

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,38 +9,30 @@ use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
 
 class FileController extends Controller
 {
-    public function uploadChunk(Request $request)
-    {
-        $receiver = new FileReceiver("file", $request, HandlerFactory::classFromRequest($request));
-
-        // Проверяем, был ли чанк загружен
-        if ($receiver->isUploaded()) {
-            $save = $receiver->receive();
-            // Если все чанки загружены, возвращаем успешный ответ
-            if ($save->isFinished()) {
-                return response()->json(['success' => true]);
-            } else {
-                // Если есть еще чанки, возвращаем успешный ответ с данными о чанке
-                return response()->json(['success' => true, 'chunk_index' => $save->getChunkFilename()]);
-            }
-        }
-
-        // Если не был загружен ни один чанк, возвращаем сообщение об ошибке
-        return response()->json(['success' => false]);
-    }
-
     public function upload(Request $request)
-    {
-        // Обработка загрузки файла после того, как все чанки были загружены
-        $receiver = new FileReceiver("file", $request, HandlerFactory::classFromRequest($request));
-        $save = $receiver->receive();
-        if ($save->isFinished()) {
-            // Сохраняем файл после того, как все чанки были загружены
-            $path = $save->getFile();
-            $path->store('uploads'); // Переменные для сохранения в базу данных
-            return response()->json(['success' => true]);
-        }
+{
+    $receiver = new FileReceiver("file", $request, HandlerFactory::classFromRequest($request));
 
-        return response()->json(['success' => false]);
+    // Проверяем, был ли файл загружен
+    if ($receiver->isUploaded() === false) {
+        throw new UploadMissingFileException();
     }
+
+    // Получаем сохраненный файл
+    $save = $receiver->receive();
+
+    // Проверяем, завершена ли загрузка
+    if ($save->isFinished()) {
+        // Сохраняем файл и возвращаем ответ
+        return $this->saveFile($save->getFile());
+    }
+
+    // Отправляем текущий прогресс загрузки
+    $handler = $save->handler();
+
+    return response()->json([
+        "done" => $handler->getPercentageDone(),
+        'status' => true
+    ]);
+}
 }
