0fd641111096b5f04d57a6eb0d8a808c8433fcc0
rayde03 <barchugova03@bk.ru>
2024-04-19 10:59:54 +0700

    3

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,43 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Pion\Laravel\ChunkUpload\Exceptions\UploadMissingFileException;
-use Pion\Laravel\ChunkUpload\Handler\HandlerFactory;
-use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
-
-class FileController extends Controller
+class FileController extends
 {
-    public function upload(Request $request)
-{
-    $receiver = new FileReceiver("file", $request, HandlerFactory::classFromRequest($request));
-
-    // Проверяем, был ли файл загружен
-    if ($receiver->isUploaded() === false) {
-        throw new UploadMissingFileException();
-    }
-
-    // Получаем сохраненный файл
-    $save = $receiver->receive();
 
     // Проверяем, была ли успешно сохранена часть файла
     if ($save === false) {
         throw new UploadFailedException(); // Возможно, вы захотите создать своё исключение UploadFailedException
     }
 
-    // Проверяем, завершена ли загрузка
-    if ($save->isFinished()) {
-        // Сохраняем файл и возвращаем ответ
-        return $this->saveFile($save->getFile());
-    }
-
-    // Отправляем текущий прогресс загрузки
-    $handler = $save->handler();
-
-    return response()->json([
-        "done" => $handler->getPercentageDone(),
-        'status' => true
-    ]);
-}
 }
