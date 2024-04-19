63946e2743cd54a89b1fad2f7e163ae5dbc70884
Constantine <81346637+Zavodsk0y@users.noreply.github.com>
2024-04-19 07:17:35 +0300

    Update FileController.php

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -10,35 +10,39 @@ use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
 
 class FileController extends Controller
 {
-    public function upload(Request $request)
-{
-    $receiver = new FileReceiver("file", $request, HandlerFactory::classFromRequest($request));
+    public function upload(Request $request) {
+        // create the file receiver
+        $receiver = new FileReceiver("file", $request, HandlerFactory::classFromRequest($request));
 
-    // Проверяем, был ли файл загружен
-    if ($receiver->isUploaded() === false) {
-        throw new UploadMissingFileException();
+        // check if the upload is success, throw exception or return response you need
+        if ($receiver->isUploaded() === false) {
+            throw new UploadMissingFileException();
+        }
+
+        // receive the file
+        $save = $receiver->receive();
+
+        // check if the upload has finished (in chunk mode it will send smaller files)
+        if ($save->isFinished()) {
+            // save the file and return any response you need, current example uses `move` function. If you are
+            // not using move, you need to manually delete the file by unlink($save->getFile()->getPathname())
+            return $this->saveFile($save->getFile());
+        }
+
+        // we are in chunk mode, lets send the current progress
+        /** @var AbstractHandler $handler */
+        $handler = $save->handler();
+
+        return response()->json([
+            "done" => $handler->getPercentageDone(),
+            'status' => true
+        ]);
     }
 
-    // Получаем сохраненный файл
-    $save = $receiver->receive();
-
-    // Проверяем, была ли успешно сохранена часть файла
-    if ($save === false) {
-        throw new UploadFailedException(); // Возможно, вы захотите создать своё исключение UploadFailedException
+    private function saveFile($file) {
+        // Implement your file saving logic here
+        return response()->json([
+            "success" => true
+        ]);
     }
-
-    // Проверяем, завершена ли загрузка
-    if ($save->isFinished()) {
-        // Сохраняем файл и возвращаем ответ
-        return $this->saveFile($save->getFile());
-    }
-
-    // Отправляем текущий прогресс загрузки
-    $handler = $save->handler();
-
-    return response()->json([
-        "done" => $handler->getPercentageDone(),
-        'status' => true
-    ]);
-}
 }
